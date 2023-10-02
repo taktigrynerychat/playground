21332da968308b80e096969ff94dd57794f6cf28
Vlad <vladdaking@gmail.com>
2023-10-02 16:55:43 +0300

    Fabric pattern

---
+++ packages/patterns/creational/fabric/index.ts
@@ -0,0 +1,72 @@
+abstract class Car {
+  protected _assemblingDate: Date;
+
+  constructor() {
+    this._assemblingDate = new Date()
+  }
+
+  public abstract cost: number;
+  public abstract honk(): void;
+  public abstract brand: string;
+  public abstract get assemblingDate(): string;
+}
+
+class ToyotaCar extends Car {
+  public brand = 'Toyota'
+  public cost = 3000
+
+  public get assemblingDate(): string {
+    return `${this._assemblingDate.getDay() + 1}/${this._assemblingDate.getMonth() + 1}/${this._assemblingDate.getFullYear()}`;
+  }
+
+  public honk(): void {
+    console.log(`${this.brand} honk honk 😊`)
+  }
+}
+
+class NisanCar extends Car {
+  public brand = 'Nisan'
+  public cost = 3400
+
+  public get assemblingDate(): string {
+    return `${this._assemblingDate.getDay() + 1}.${this._assemblingDate.getMonth() + 1}.${this._assemblingDate.getFullYear()}`;
+  }
+
+  public honk(): void {
+    console.log(`${this.brand} honk honk (❁´◡\`❁`)
+  }
+}
+
+class LadaCar {
+  public brand = 'Lada'
+  public cost = 50
+
+  public get assemblingDate(): string {
+    return `tomorrow`;
+  }
+
+  public honk(): void {
+    console.log('Very good car beep beep')
+  }
+}
+
+function carFactory<T extends Car>(brand: new() => T): T {
+  const factoryFeePerCar = 100;
+
+  const car = new brand()
+  if (!(car instanceof Car)) {
+    throw `${brand.name} could not be constructed on this fabric!`
+  }
+  car.cost += factoryFeePerCar
+
+  return car;
+}
+
+
+
+export default (): void => {
+  const nisanCar = carFactory(NisanCar)
+  const toyotaCar = carFactory(ToyotaCar)
+
+  console.log(nisanCar, toyotaCar)
+}
--- packages/patterns/main.ts
+++ packages/patterns/main.ts
@@ -1,8 +1,28 @@
+const patterns = {
+  fabric: {
+    dynamicImport: import('./creational/fabric'),
+    executeByDefault: true
+  }
+}
+
 class AppElement extends HTMLElement {
   public static observedAttributes = [];
 
   connectedCallback() {
-    this.innerHTML = `<h1>Hello world!</h1>`;
+    Object.entries(patterns).forEach(([key, {dynamicImport, executeByDefault}]) => {
+      const el = document.createElement('button')
+      const callback = async () => {
+        const { default: main } = await dynamicImport
+        main()
+      }
+
+      el.innerHTML = key
+      el.onclick = callback
+      if (executeByDefault) {
+        callback()
+      }
+      this.appendChild(el)
+    })
   }
 }
 
