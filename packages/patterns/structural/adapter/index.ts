c79556ed0d7db2fb3677b7142b7a8085f844cf3e
Vlad <vladdaking@gmail.com>
2023-10-04 16:41:30 +0300

    Adapter pattern

--- packages/patterns/structural/adapter/index.ts
+++ packages/patterns/structural/adapter/index.ts
@@ -1,3 +1,35 @@
-export default () => {
+interface UserDto {
+  name: string,
+  lastName: string,
+  age: number
+}
 
+interface User {
+  fullName: string,
+  age: number
+}
+
+abstract class Api {
+  public static getUser(): UserDto {
+    return {
+      name: 'Vlad',
+      lastName: 'Rusakov',
+      age: 25
+    }
+  }
+}
+
+abstract class Adapter {
+  public static getUser(): User {
+    const {name, lastName, age} = Api.getUser();
+    return {
+      fullName: `${name} ${lastName}`,
+      age
+    }
+  }
+}
+export default () => {
+  const userDto: UserDto = Api.getUser();
+  const user: User = Adapter.getUser();
+  console.log({user, userDto});
 }
