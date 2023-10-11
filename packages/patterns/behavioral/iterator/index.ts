574300d7076504e75fcae0bb5283d1a3c7f32546
Vlad <vladdaking@gmail.com>
2023-10-11 12:05:05 +0300

    Iterator pattern second revision

--- packages/patterns/behavioral/iterator/index.ts
+++ packages/patterns/behavioral/iterator/index.ts
@@ -1,39 +1,95 @@
-interface Range {
-  from: number;
-  to: number;
-  [Symbol.iterator](): Iterator<number>;
+interface RangeIterator {
+  (this: Range): Iterator<number>;
 }
+class Range {
+  constructor(
+    public readonly from: number,
+    public readonly to: number,
+    private _iteratorFunction: RangeIterator = iteratorFn,
+  ) {}
 
-function getRange(from: number, to: number): Range {
-  return {
-    from,
-    to,
-    [Symbol.iterator]() {
-      let current = this.from;
-      let last = this.to;
+  public [Symbol.iterator](): Iterator<number> {
+    return this._iteratorFunction();
+  }
 
-      return {
-        next() {
-          if (current <= last) {
-            return {
-              done: false,
-              value: current++
-            };
-          } else {
-            return {
-              done: true,
-              value: undefined
-            };
-          }
-        }
+  public setIterator(iteratorFunction: RangeIterator): this {
+    this._iteratorFunction = iteratorFunction;
 
-      }
-    }
-
+    return this;
   }
 }
+
+function iteratorFn(this: Range): Iterator<number> {
+  let self = this;
+  let current = self.from;
+  return {
+    next() {
+      if (current <= self.to) {
+        return {
+          done: false,
+          value: current++
+        };
+      } else {
+        self = null;
+        current = null;
+        return {
+          done: true,
+          value: undefined
+        };
+      }
+    }
+  }
+}
+
+function reverseIteratorFn(this: Range): Iterator<number> {
+  let self = this;
+  let current = self.to;
+  return {
+    next() {
+      if (current >= self.from) {
+        return {
+          done: false,
+          value: current--
+        };
+      } else {
+        self = null;
+        current = null;
+        return {
+          done: true,
+          value: undefined
+        };
+      }
+    }
+  }
+}
+
+function customIterator(range: Range) {
+  let current = range.to;
+  return {
+    valid() {
+      const isValid= current >= range.from;
+      if (!isValid) current = null;
+      return isValid;
+    },
+    next() {
+      if (current >= range.from) {
+        return current--
+      } else {
+        return undefined
+      }
+    }
+  }
+}
+
 export default () => {
-  const range = getRange(1, 10);
-  console.log(range)
+  const range = new Range(1, 10);
+  console.log(range);
   console.log(...range)
+  range.setIterator(reverseIteratorFn)
+  console.log(...range)
+
+  const iterator = customIterator(range)
+  while (iterator.valid()) {
+    console.log(iterator.next())
+  }
 }
